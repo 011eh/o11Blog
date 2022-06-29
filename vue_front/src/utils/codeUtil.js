a13ca9c596e15d7c0c3055da78d50ebfcf41246a
011eh <1395663435@qq.com>
2022-06-29 21:30:24 +0800

    准备注册接口

---
+++ vue_front/src/utils/codeUtil.js
@@ -0,0 +1,26 @@
+import {Notification} from "element-ui";
+
+function showMsg(code) {
+  return Math.floor(code / 1000) === 1;
+}
+
+function success(code) {
+  return code % 1000 === 200;
+}
+
+function getCode(code) {
+  return code % 1000;
+}
+
+function successMsg(res) {
+  if (success(res.code) && showMsg(res.code)) {
+    Notification({
+      title: '成功',
+      message: res.msg || '操作成功',
+      type: 'success'
+    })
+  }
+}
+
+export {getCode,success, showMsg, successMsg}
+
